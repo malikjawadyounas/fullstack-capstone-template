17b01f310d9870e6e078f63d65caa07c27eb653b
malikjawadyounas <jawadmalik6467@gmail.com>
2026-09-15 11:46:56 +0500

    Add registration API call in register component

--- giftlink-frontend/src/components/register.js
+++ giftlink-frontend/src/components/register.js
@@ -1,1 +1,15 @@
 
+const response = await fetch(
+    `${urlConfig.backendUrl}/api/auth/register`,
+    {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            name,
+            email,
+            password
+        })
+    }
+);
